8fdfa30c6ec81504859b3993fef7a0af2ca58cc6
VISHAL SINGH <daggercoding@gmail.com>
2024-02-14 23:53:11 +0530

    write inidpendent js file to read a file and push on mongodb

--- ExpressJs/data/indipendent.js
+++ ExpressJs/data/indipendent.js
@@ -1,13 +1,20 @@
+//importing different modules which is necessary for 
+// mongoose connection to database
+// reqquiring dotenv file
+// fs module for reading the file
+// Movie is our model which we created and exported
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const fs = require("fs")
 const Movie = require("../Models/MovieModel")
 
-
+//config the .env file
 dotenv.config()
 
-const Movies = fs.readFileSync("./data/Movies.json","utf-8")
+//converting the js object in BSON formal
+const Movies = JSON.parse(fs.readFileSync("./data/Movies.json","utf-8"))
 
+//connecting the databse to js file
 mongoose.connect(process.env.DB_COLLECTION_STRING)
 .then(()=>{
     console.log("Db Conected")
@@ -16,6 +23,7 @@ mongoose.connect(process.env.DB_COLLECTION_STRING)
   console.log(err.message)
 })
 
+//function to delete all the movies
 const deleteMovies = async()=>{
    try{
     await Movie.deleteMany()
@@ -23,4 +31,28 @@ const deleteMovies = async()=>{
    }catch(err){
     console.log(err)
    }
+   process.exit()
+}
+
+//function to import all the movies
+const importMovies = async()=>{
+    try{
+     await Movie.create(Movies)
+     console.log("Movie Exported sucessfully")
+    }catch(err){
+     console.log(err)
+    }
+    process.exit()
+ }
+
+//running delete and import function on the basis of some condition
+console.log(process.argv)
+if(process.argv[2]=="--delete")
+{
+    deleteMovies()
+}
+
+if(process.argv[2]=="--import")
+{
+    importMovies()
 }
